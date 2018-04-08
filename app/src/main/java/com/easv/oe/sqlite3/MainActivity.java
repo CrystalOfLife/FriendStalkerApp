7856853a1e3cc28fa5a095cc9bc6fc64857f355b
kingmain1 <nissenjens@hotmail.com>
2018-04-08 12:27:40 +0200

    making menu.
    trying to get it to work

--- app/src/main/java/com/easv/oe/sqlite3/MainActivity.java
+++ app/src/main/java/com/easv/oe/sqlite3/MainActivity.java
@@ -4,6 +4,7 @@ package com.easv.oe.sqlite3;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,7 +12,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     EditText etName;
     Button insertButton;
